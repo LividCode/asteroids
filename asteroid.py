1081cbb94d628092bcef30af2c1f715a5e11dd6a
LividCode <william.l.yeo@gmail.com>
2026-04-21 20:57:50 +0100

    added collision and shooting

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
+import random
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +13,20 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         self.position += (self.velocity * dt)
+    
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        log_event("asteroid_split")
+        random_angle = random.uniform(20, 50)
+        child_asteroid_velocity1 = self.velocity.rotate(random_angle)
+        child_asteroid_velocity2 = self.velocity.rotate(-abs(random_angle))
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        child_asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        child_asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+        child_asteroid1.velocity = child_asteroid_velocity1 * 1.2
+        child_asteroid2.velocity = child_asteroid_velocity2 * 1.2
